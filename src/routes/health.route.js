f55d5b52cb75e2219369461c013864b6a402b4b8
rakeshsolankingts-stack <rakesh.solankingts@gmail.com>
2026-03-30 11:51:41 +0530

    "cron appdate"

--- src/routes/health.route.js
+++ src/routes/health.route.js
@@ -2,12 +2,16 @@
 const express = require('express');
 const router  = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
+  try{
   res.status(200).json({
     status   : 'OK',
     uptime   : process.uptime(),       // server kitne seconds se chal raha hai
     timestamp: new Date().toISOString()
   });
+}catch(error){
+  next(error)
+}
 });
 
 module.exports = router;
